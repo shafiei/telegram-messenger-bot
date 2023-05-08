f65437aba865ca595cd7fe5d9e177512127b2655
Mohammad Reza Shafiei <shafieipro@gmail.com>
2023-05-08 06:11:30 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Robot source by https://shafiei.dev
+// just replace robot Admin Telegram ID number in "00000000" on line 6 , and paste bot token(from botfather) between "" on line7
+
+$Dev = 00000000; //ایدی ادمین
+$Token = ""; //توکن ربات
+
+?>
